826a90ca7cc60eec7b5b9c3f5028a9fffa82efb2
Alpha2546 <adriaanvanloon@hotmail.com>
2015-09-27 18:53:50 +0200

    removing the uncessary files and adding andreasg changes

---
+++ ui/mods/andreasg/maplist.js
@@ -0,0 +1,6 @@
+﻿cShareSystems.load_pas("AndreasG", [
+	"coui://ui/mods/andreasg/systems/tectonic.pas",	
+	"coui://ui/mods/andreasg/systems/progenitors_lair.pas",
+	"coui://ui/mods/andreasg/systems/hanfyren.pas",	
+	"coui://ui/mods/andreasg/systems/xankars™_fury commpush_© _ ©.pas"		
+]);

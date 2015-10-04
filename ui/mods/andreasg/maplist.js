a95f0034336112008ae693a1fd470224d483924d
Alpha2546 <adriaanvanloon@hotmail.com>
2015-10-04 17:30:45 +0200

    fixing a typo plus updating version number

--- ui/mods/andreasg/maplist.js
+++ ui/mods/andreasg/maplist.js
@@ -1,7 +1,7 @@
 ﻿cShareSystems.load_pas("AndreasG", [
 	"coui://ui/mods/andreasg/systems/the_forgery.pas",	
 	"coui://ui/mods/andreasg/systems/chocke.pas",
-	"coui://ui/mods/andreasg/systems/the_faceoff",	
+	"coui://ui/mods/andreasg/systems/the_faceoff.pas",	
 	"coui://ui/mods/andreasg/systems/gsystem.pas",	
 	"coui://ui/mods/andreasg/systems/8_leaf_clover.pas",	
 	"coui://ui/mods/andreasg/systems/mots_recipe_for_disaster.pas"		

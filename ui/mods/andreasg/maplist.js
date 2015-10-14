d7fe909af02fbf9bbb7a938ec9115d7c193e4333
PAAndreasG <greieandreas@hotmail.com>
2015-10-14 21:43:27 +0200

    2 Map update
    FrozenOreoYoghurt and AmazonSafari added to the map pack. Minor update
    on The Forgery as well

--- ui/mods/andreasg/maplist.js
+++ ui/mods/andreasg/maplist.js
@@ -1,4 +1,6 @@
-﻿cShareSystems.load_pas("AndreasG", [
+﻿cShareSystems.load_pas("AndreasG 1v1 Pack", [
+	"coui://ui/mods/andreasg/systems/frozenoreoyoghurt",
+	"coui://ui/mods/andreasg/systems/amazonassafari",
 	"coui://ui/mods/andreasg/systems/the_forgery.pas",	
 	"coui://ui/mods/andreasg/systems/chocke.pas",
 	"coui://ui/mods/andreasg/systems/the_faceoff.pas",	

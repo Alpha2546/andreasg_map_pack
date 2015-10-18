d9c142a0d92e0de0c42276a1227427bcf01a1963
PAAndreasG <greieandreas@hotmail.com>
2015-10-18 18:22:15 +0200

    1.4.2
    bug fixes

--- ui/mods/andreasg/maplist.js
+++ ui/mods/andreasg/maplist.js
@@ -1,6 +1,6 @@
 ﻿cShareSystems.load_pas("AndreasG 1v1 Pack", [
-	"coui://ui/mods/andreasg/systems/frozenoreoyoghurt",
-	"coui://ui/mods/andreasg/systems/amazonassafari",
+	"coui://ui/mods/andreasg/systems/frozenoreoyoghurt.pas",
+	"coui://ui/mods/andreasg/systems/amazonassafari.pas",
 	"coui://ui/mods/andreasg/systems/the_forgery.pas",	
 	"coui://ui/mods/andreasg/systems/chocke.pas",
 	"coui://ui/mods/andreasg/systems/the_faceoff.pas",	

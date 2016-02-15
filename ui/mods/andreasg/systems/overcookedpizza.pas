236f59e6a1669a0350d7e7352a0566afb2c5f294
PAAndreasG <greieandreas@hotmail.com>
2016-02-15 14:18:23 +0100

    2.1.1
    Bugfixes

--- ui/mods/andreasg/systems/overcookedpizza.pas
+++ ui/mods/andreasg/systems/overcookedpizza.pas
@@ -4,8 +4,7 @@
 	"version": "1.0",
 	"date": "2016/02/15",
 	"description": "We all love Pizza, but not overcooked. You are going to love this one anyways. It is a handsome map with close combat and sneaky expantions. Watch out for AsZeals air fabbers.",
-
-    "planets": [
+	"planets": [
         {
             "name": BurntPizza",
             "mass": 10000,
